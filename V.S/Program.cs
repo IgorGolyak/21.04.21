15b6e6bed2e672979d080886309b6b9f32f1d3c1
Игорь Голяк <golyak.03@inbox.ru>
2021-04-21 11:18:34 +0700

    21.04.21

---
+++ V.S/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V.S
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("1 чэтверт");
+                }
+                else
+                {
+                    Console.WriteLine("4 чэтверт");
+                }
+
+            }
+            else
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("2 чэтверт");
+                }
+                else
+                {
+                    Console.WriteLine("3 чэтверт");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
